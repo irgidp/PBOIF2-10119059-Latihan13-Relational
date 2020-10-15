b64c203bfe255a594f0ac9c448a29e602dd2d167
irgidp <irgijak@gmail.com>
2020-10-15 22:25:57 +0700

    Latihan 13

---
+++ PBOIF210119059Latihan13Relational.java
@@ -0,0 +1,26 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan relational/perbandingan
+ */
+package pboif2.pkg10119059.latihan13.relational;
+
+public class PBOIF210119059Latihan13Relational {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+
+    }
+    
+}
